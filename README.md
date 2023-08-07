ade6ca3549a2eae9dd00e64cdcaf6f1e115aaf53
Shanti <fishfinna12@gmail.com>
2023-08-07 15:50:59 -0700

    added more env stuff

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # readpy
+
 Converts PDFs into audio books to read on the go
+
+The external libraries needed for this are:
+
+- pypdf: used to read from pdf files
+- requests: used to send API requests to and from Lovo AI
+
+the required packages can be installed with the following commands
+
+```
+pip install pypdf
+pip install requests
+```
+
+Since this project relies on the external Lovo API you will need to get an API key
+to do this visit the `https://genny.lovo.ai/` website, create an account, and then click on your profile to view and generate API keys.
+
+Once generated you can run the script as normal and input the API key when prompted.
--- readpy.py
+++ readpy.py
@@ -1,28 +1,42 @@
 from pypdf import PdfReader
+import requests
 import os
+api_key_env_var = "API_KEY"
+env_file_path = ".env"
 
 def main(): 
-    pdf_file = ""
-    while not pdf_file:
+
+    if os.path.exists(env_file_path):
+        with open(env_file_path, "r") as env_file:
+            env_contents = env_file.read()
+    else:
+        env_contents = ""
+    if f"{api_key_env_var}=" not in env_contents:
+        api_key = input("No API key found! Please enter your API Lovo AI key:")
+
+        with open(env_file_path, "w") as file:
+            file.write(f"{api_key_env_var}={api_key}")
+            print("your API key has been stored to {}".format(env_file_path))
+
+
+    pdf_file_name = ""
+    while not pdf_file_name:
         input_file = input("Please enter the name of your PDF file: ").lower()
 
         # make sure it is a PDF file
         if os.path.splitext(input_file)[1] != ".pdf":
             input_file = input_file + ".pdf"
 
-
-        # check to make sure the file exists
         if os.path.isfile(f"{os.getcwd()}\\{input_file}"):
-            pdf_file = input_file
+            pdf_file_name = input_file
             print("file found!")
         else:
             print("file not found! please make sure the pdf file exists in your current directory and try again.")
 
-    reader = PdfReader(pdf_file)
-    for page in reader.pages:
-        print(page.extract_text())
-
-        #TODO: add the text to speech
+    reader = PdfReader(pdf_file_name)
+    pdf_text = ("".join([page.extract_text().replace("\n", " ") for page in reader.pages]))
+    print(pdf_text)
+    
 
 if __name__ == "__main__":
     main()
